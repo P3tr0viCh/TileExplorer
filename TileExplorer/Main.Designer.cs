ec97bd9be8f117908fc6c3512da6014d3055f29c
Константин Петрович <P3tr0viCh@users.noreply.github.com>
2023-03-23 17:25:21 +0500

    Add project files.

---
+++ TileExplorer/Main.Designer.cs
@@ -0,0 +1,248 @@
+﻿namespace TileExplorer
+{
+    partial class Main
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.gMapControl = new GMap.NET.WindowsForms.GMapControl();
+            this.statusStrip = new System.Windows.Forms.StatusStrip();
+            this.slZoom = new System.Windows.Forms.ToolStripStatusLabel();
+            this.slPosition = new System.Windows.Forms.ToolStripStatusLabel();
+            this.menuStrip = new System.Windows.Forms.MenuStrip();
+            this.miMainFile = new System.Windows.Forms.ToolStripMenuItem();
+            this.miMainClose = new System.Windows.Forms.ToolStripMenuItem();
+            this.miMainMap = new System.Windows.Forms.ToolStripMenuItem();
+            this.miMainFullScreen = new System.Windows.Forms.ToolStripMenuItem();
+            this.miMainSaveToImage = new System.Windows.Forms.ToolStripMenuItem();
+            this.miMainHelp = new System.Windows.Forms.ToolStripMenuItem();
+            this.miMainAbout = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripContainer = new System.Windows.Forms.ToolStripContainer();
+            this.saveFileDialog = new System.Windows.Forms.SaveFileDialog();
+            this.statusStrip.SuspendLayout();
+            this.menuStrip.SuspendLayout();
+            this.toolStripContainer.BottomToolStripPanel.SuspendLayout();
+            this.toolStripContainer.ContentPanel.SuspendLayout();
+            this.toolStripContainer.TopToolStripPanel.SuspendLayout();
+            this.toolStripContainer.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // gMapControl
+            // 
+            this.gMapControl.Bearing = 0F;
+            this.gMapControl.CanDragMap = true;
+            this.gMapControl.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.gMapControl.EmptyTileColor = System.Drawing.Color.Navy;
+            this.gMapControl.GrayScaleMode = false;
+            this.gMapControl.HelperLineOption = GMap.NET.WindowsForms.HelperLineOptions.DontShow;
+            this.gMapControl.LevelsKeepInMemmory = 5;
+            this.gMapControl.Location = new System.Drawing.Point(0, 0);
+            this.gMapControl.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.gMapControl.MarkersEnabled = true;
+            this.gMapControl.MaxZoom = 2;
+            this.gMapControl.MinZoom = 2;
+            this.gMapControl.MouseWheelZoomEnabled = true;
+            this.gMapControl.MouseWheelZoomType = GMap.NET.MouseWheelZoomType.MousePositionAndCenter;
+            this.gMapControl.Name = "gMapControl";
+            this.gMapControl.NegativeMode = false;
+            this.gMapControl.PolygonsEnabled = true;
+            this.gMapControl.RetryLoadTile = 0;
+            this.gMapControl.RoutesEnabled = true;
+            this.gMapControl.ScaleMode = GMap.NET.WindowsForms.ScaleModes.Integer;
+            this.gMapControl.SelectedAreaFillColor = System.Drawing.Color.FromArgb(((int)(((byte)(33)))), ((int)(((byte)(65)))), ((int)(((byte)(105)))), ((int)(((byte)(225)))));
+            this.gMapControl.ShowTileGridLines = false;
+            this.gMapControl.Size = new System.Drawing.Size(584, 415);
+            this.gMapControl.TabIndex = 0;
+            this.gMapControl.Zoom = 0D;
+            this.gMapControl.OnMarkerClick += new GMap.NET.WindowsForms.MarkerClick(this.gMapControl_OnMarkerClick);
+            this.gMapControl.OnPositionChanged += new GMap.NET.PositionChanged(this.GMapControl_OnPositionChanged);
+            this.gMapControl.OnMapZoomChanged += new GMap.NET.MapZoomChanged(this.GMapControl_OnMapZoomChanged);
+            this.gMapControl.Load += new System.EventHandler(this.GMapControl_Load);
+            // 
+            // statusStrip
+            // 
+            this.statusStrip.Dock = System.Windows.Forms.DockStyle.None;
+            this.statusStrip.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.slZoom,
+            this.slPosition});
+            this.statusStrip.Location = new System.Drawing.Point(0, 0);
+            this.statusStrip.Name = "statusStrip";
+            this.statusStrip.Size = new System.Drawing.Size(584, 22);
+            this.statusStrip.TabIndex = 1;
+            // 
+            // slZoom
+            // 
+            this.slZoom.Name = "slZoom";
+            this.slZoom.Size = new System.Drawing.Size(61, 17);
+            this.slZoom.Text = "zoom: xxx";
+            // 
+            // slPosition
+            // 
+            this.slPosition.Name = "slPosition";
+            this.slPosition.Size = new System.Drawing.Size(62, 17);
+            this.slPosition.Text = "pos: xx, yy";
+            // 
+            // menuStrip
+            // 
+            this.menuStrip.Dock = System.Windows.Forms.DockStyle.None;
+            this.menuStrip.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.miMainFile,
+            this.miMainMap,
+            this.miMainHelp});
+            this.menuStrip.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip.Name = "menuStrip";
+            this.menuStrip.Size = new System.Drawing.Size(584, 24);
+            this.menuStrip.TabIndex = 2;
+            this.menuStrip.Text = "menuStrip1";
+            // 
+            // miMainFile
+            // 
+            this.miMainFile.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.miMainClose});
+            this.miMainFile.Name = "miMainFile";
+            this.miMainFile.Size = new System.Drawing.Size(48, 20);
+            this.miMainFile.Text = "Файл";
+            // 
+            // miMainClose
+            // 
+            this.miMainClose.Name = "miMainClose";
+            this.miMainClose.Size = new System.Drawing.Size(120, 22);
+            this.miMainClose.Text = "Закрыть";
+            this.miMainClose.Click += new System.EventHandler(this.CloseToolStripMenuItem_Click);
+            // 
+            // miMainMap
+            // 
+            this.miMainMap.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.miMainFullScreen,
+            this.miMainSaveToImage});
+            this.miMainMap.Name = "miMainMap";
+            this.miMainMap.Size = new System.Drawing.Size(50, 20);
+            this.miMainMap.Text = "Карта";
+            // 
+            // miMainFullScreen
+            // 
+            this.miMainFullScreen.Name = "miMainFullScreen";
+            this.miMainFullScreen.ShortcutKeys = System.Windows.Forms.Keys.F11;
+            this.miMainFullScreen.Size = new System.Drawing.Size(232, 22);
+            this.miMainFullScreen.Text = "Полноэкранный режим";
+            this.miMainFullScreen.Click += new System.EventHandler(this.MiMainFullScreen_Click);
+            // 
+            // miMainSaveToImage
+            // 
+            this.miMainSaveToImage.Name = "miMainSaveToImage";
+            this.miMainSaveToImage.Size = new System.Drawing.Size(232, 22);
+            this.miMainSaveToImage.Text = "Сохранить в файл...";
+            this.miMainSaveToImage.Click += new System.EventHandler(this.SaveToImageToolStripMenuItem_Click);
+            // 
+            // miMainHelp
+            // 
+            this.miMainHelp.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.miMainAbout});
+            this.miMainHelp.Name = "miMainHelp";
+            this.miMainHelp.Size = new System.Drawing.Size(65, 20);
+            this.miMainHelp.Text = "Справка";
+            // 
+            // miMainAbout
+            // 
+            this.miMainAbout.Name = "miMainAbout";
+            this.miMainAbout.Size = new System.Drawing.Size(180, 22);
+            this.miMainAbout.Text = "О программе...";
+            this.miMainAbout.Click += new System.EventHandler(this.MiMainAbout_Click);
+            // 
+            // toolStripContainer
+            // 
+            // 
+            // toolStripContainer.BottomToolStripPanel
+            // 
+            this.toolStripContainer.BottomToolStripPanel.Controls.Add(this.statusStrip);
+            // 
+            // toolStripContainer.ContentPanel
+            // 
+            this.toolStripContainer.ContentPanel.AutoScroll = true;
+            this.toolStripContainer.ContentPanel.Controls.Add(this.gMapControl);
+            this.toolStripContainer.ContentPanel.Size = new System.Drawing.Size(584, 415);
+            this.toolStripContainer.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.toolStripContainer.Location = new System.Drawing.Point(0, 0);
+            this.toolStripContainer.Name = "toolStripContainer";
+            this.toolStripContainer.Size = new System.Drawing.Size(584, 461);
+            this.toolStripContainer.TabIndex = 3;
+            // 
+            // toolStripContainer.TopToolStripPanel
+            // 
+            this.toolStripContainer.TopToolStripPanel.Controls.Add(this.menuStrip);
+            // 
+            // saveFileDialog
+            // 
+            this.saveFileDialog.DefaultExt = "png";
+            this.saveFileDialog.Filter = "PNG|*.png|Все файлы|*.*";
+            // 
+            // Main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(584, 461);
+            this.Controls.Add(this.toolStripContainer);
+            this.Font = new System.Drawing.Font("Segoe UI", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.MainMenuStrip = this.menuStrip;
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Name = "Main";
+            this.Text = "TileExplorer";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Main_FormClosing);
+            this.Load += new System.EventHandler(this.Main_Load);
+            this.statusStrip.ResumeLayout(false);
+            this.statusStrip.PerformLayout();
+            this.menuStrip.ResumeLayout(false);
+            this.menuStrip.PerformLayout();
+            this.toolStripContainer.BottomToolStripPanel.ResumeLayout(false);
+            this.toolStripContainer.BottomToolStripPanel.PerformLayout();
+            this.toolStripContainer.ContentPanel.ResumeLayout(false);
+            this.toolStripContainer.TopToolStripPanel.ResumeLayout(false);
+            this.toolStripContainer.TopToolStripPanel.PerformLayout();
+            this.toolStripContainer.ResumeLayout(false);
+            this.toolStripContainer.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private GMap.NET.WindowsForms.GMapControl gMapControl;
+        private System.Windows.Forms.StatusStrip statusStrip;
+        private System.Windows.Forms.ToolStripStatusLabel slZoom;
+        private System.Windows.Forms.ToolStripStatusLabel slPosition;
+        private System.Windows.Forms.MenuStrip menuStrip;
+        private System.Windows.Forms.ToolStripMenuItem miMainFile;
+        private System.Windows.Forms.ToolStripMenuItem miMainClose;
+        private System.Windows.Forms.ToolStripMenuItem miMainMap;
+        private System.Windows.Forms.ToolStripMenuItem miMainSaveToImage;
+        private System.Windows.Forms.ToolStripContainer toolStripContainer;
+        private System.Windows.Forms.ToolStripMenuItem miMainFullScreen;
+        private System.Windows.Forms.ToolStripMenuItem miMainHelp;
+        private System.Windows.Forms.ToolStripMenuItem miMainAbout;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog;
+    }
+}
+
---
+++ TileExplorer/Main.cs
@@ -0,0 +1,315 @@
+﻿using P3tr0viCh;
+
+using GMap.NET;
+using GMap.NET.WindowsForms;
+using GMap.NET.WindowsForms.Markers;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Windows.Forms;
+using TileExplorer.Properties;
+using System.Reflection;
+
+namespace TileExplorer
+{
+    public partial class Main : Form
+    {
+        GMapOverlay tiles;
+        GMapOverlay images;
+        GMapOverlay markers;
+
+        public const int TILE_ZOOM = 14;
+
+        public Main()
+        {
+            InitializeComponent();
+        }
+
+        private void Main_Load(object sender, EventArgs e)
+        {
+            var mainBounds = Settings.Default.MainBounds;
+
+            if (mainBounds.X < 0) mainBounds.X = (Screen.FromControl(this).WorkingArea.Width - Width) / 2;
+            if (mainBounds.Y < 0) mainBounds.Y = (Screen.FromControl(this).WorkingArea.Height - Height) / 2;
+            if (mainBounds.Width < Width) mainBounds.Width = Width;
+            if (mainBounds.Height < Height) mainBounds.Height = Height;
+
+            Bounds = mainBounds;
+
+            if (Settings.Default.MainMaximized)
+            {
+                WindowState = FormWindowState.Maximized;
+            }
+        }
+
+        private void Main_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            SetFullScreen(false);
+
+            if (WindowState == FormWindowState.Maximized)
+            {
+                Settings.Default.MainMaximized = true;
+            }
+            else
+            {
+                Settings.Default.MainMaximized = false;
+                Settings.Default.MainBounds = Bounds;
+            }
+
+            Settings.Default.Save();
+        }
+
+        private void GMapControl_Load(object sender, EventArgs e)
+        {
+            GMaps.Instance.Mode = AccessMode.ServerAndCache;
+            //gMapControl.CacheLocation = System.IO.Path.GetDirectoryName(Application.ExecutablePath);
+
+            GMap.NET.MapProviders.GMapProvider.UserAgent = "xxx/1.0";
+
+            gMapControl.MapProvider = GMap.NET.MapProviders.OpenStreetMapProvider.Instance;
+            gMapControl.MapProvider.RefererUrl = "debug";
+
+            gMapControl.ShowCenter = false;
+
+            gMapControl.MinZoom = 2;
+            gMapControl.MaxZoom = 16;
+            gMapControl.Zoom = 13;
+            GMapControl_OnMapZoomChanged();
+
+            gMapControl.Position = new PointLatLng(51.1977, 58.2961);
+            GMapControl_OnPositionChanged(gMapControl.Position);
+
+            gMapControl.MouseWheelZoomType = MouseWheelZoomType.ViewCenter;
+
+            gMapControl.CanDragMap = true;
+            gMapControl.DragButton = MouseButtons.Left;
+
+            tiles = new GMapOverlay("tiles");
+            images = new GMapOverlay("images");
+            markers = new GMapOverlay("markers");
+
+            gMapControl.Overlays.Add(tiles);
+            gMapControl.Overlays.Add(images);
+            gMapControl.Overlays.Add(markers);
+
+            CreateTiles();
+            CreateImages();
+            CreateMarkers();
+        }
+
+        private void GMapControl_OnMapZoomChanged()
+        {
+            slZoom.Text = string.Format(Resources.StatusZoom, gMapControl.Zoom);
+        }
+
+        private void GMapControl_OnPositionChanged(PointLatLng point)
+        {
+            slPosition.Text = string.Format(Resources.StatusPosition, point.Lat, point.Lng);
+        }
+
+        private void CloseToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void SaveToImage(String fileName)
+        {
+            toolStripContainer.ContentPanel.SuspendLayout();
+
+            gMapControl.Dock = DockStyle.None;
+
+            var imageSize = new Size(Screen.PrimaryScreen.Bounds.Width,
+                Screen.PrimaryScreen.Bounds.Height);
+
+            try
+            {
+                gMapControl.SetBounds(
+                    (toolStripContainer.ContentPanel.Width - imageSize.Width) / 2,
+                    (toolStripContainer.ContentPanel.Height - imageSize.Height) / 2,
+                    imageSize.Width,
+                    imageSize.Height);
+
+                gMapControl.ToImage().Save(fileName, ImageFormat.Png);
+            }
+            finally
+            {
+                gMapControl.SetBounds(0, 0,
+                    toolStripContainer.ContentPanel.Width, toolStripContainer.ContentPanel.Height);
+
+                gMapControl.Dock = DockStyle.Fill;
+
+                toolStripContainer.ContentPanel.PerformLayout();
+            }
+        }
+
+        private void SaveToImageToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (saveFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                SaveToImage(saveFileDialog.FileName);
+            }
+        }
+
+        public enum TileType
+        {
+            None,
+            Visited,
+            Cluster,
+            MaxSquare
+        }
+
+        private GMapPolygon CreateTile(TileType type, PointLatLng point1, PointLatLng point2)
+        {
+            Color colorFill;
+            Color colorStroke;
+
+            switch (type)
+            {
+                case TileType.Visited:
+                    colorFill = Color.FromArgb(50, Color.Red);
+                    colorStroke = Color.FromArgb(80, Color.Red);
+                    break;
+                case TileType.Cluster:
+                    colorFill = Color.FromArgb(50, Color.Green);
+                    colorStroke = Color.FromArgb(80, Color.Green);
+                    break;
+                case TileType.MaxSquare:
+                    colorFill = Color.FromArgb(50, Color.Blue);
+                    colorStroke = Color.FromArgb(80, Color.Blue);
+                    break;
+                default:
+                    colorFill = Color.Empty;
+                    colorStroke = Color.FromArgb(100, Color.Gray);
+                    break;
+            }
+
+            return new GMapPolygon(new List<PointLatLng>
+            {
+                new PointLatLng(point1.Lat, point1.Lng),
+                new PointLatLng(point1.Lat, point2.Lng),
+                new PointLatLng(point2.Lat, point2.Lng),
+                new PointLatLng(point2.Lat, point1.Lng)
+            }, "")
+            {
+                Fill = new SolidBrush(colorFill),
+                Stroke = new Pen(colorStroke, 1)
+            };
+        }
+
+        private PointLatLng TileToPoint(int tileX, int tileY)
+        {
+            return new PointLatLng(Osm.TileYToLat(tileY, TILE_ZOOM), Osm.TileXToLng(tileX, TILE_ZOOM));
+        }
+
+        private void CreateTiles()
+        {
+            int tileX;
+            int tileY;
+
+            tileX = 10844;
+            tileY = 5470;
+            tiles.Polygons.Add(CreateTile(TileType.None, TileToPoint(tileX, tileY), TileToPoint(tileX + 1, tileY + 1)));
+
+            tileX = 10845;
+            tileY = 5470;
+            tiles.Polygons.Add(CreateTile(TileType.Visited, TileToPoint(tileX, tileY), TileToPoint(tileX + 1, tileY + 1)));
+
+            tileX = 10844;
+            tileY = 5471;
+            tiles.Polygons.Add(CreateTile(TileType.Cluster, TileToPoint(tileX, tileY), TileToPoint(tileX + 1, tileY + 1)));
+
+            tileX = 10843;
+            tileY = 5471;
+            tiles.Polygons.Add(CreateTile(TileType.MaxSquare, TileToPoint(tileX, tileY), TileToPoint(tileX + 1, tileY + 1)));
+        }
+
+        private GMapMarker CreateMarker(PointLatLng point, String Text, Point offset)
+        {
+            GMapMarker gMapMarker = new GMarkerGoogle(point, GMarkerGoogleType.blue)
+            {
+                ToolTipText = Text,
+                ToolTipMode = MarkerTooltipMode.Always
+            };
+
+            gMapMarker.ToolTip.Offset.X += offset.X;
+            gMapMarker.ToolTip.Offset.Y -= offset.Y;
+
+            return gMapMarker;
+        }
+
+        private void CreateMarkers()
+        {
+            markers.Markers.Add(CreateMarker(new PointLatLng(51.1977, 58.2961), "Яяяя", new Point(0, 0)));
+            markers.Markers.Add(CreateMarker(new PointLatLng(51.1977, 58.2981), "XXX", new Point(50, 10)));
+        }
+
+        private GMapMarker CreateImage(PointLatLng point, String fileName)
+        {
+            return new GMarkerGoogle(point, new Bitmap(fileName));
+        }
+
+        private void CreateImages()
+        {
+            images.Markers.Add(CreateImage(new PointLatLng(51.2169, 58.2446), $@"d:\!Temp\logo.png"));
+            images.Markers.Add(CreateImage(new PointLatLng(51.2132, 58.3297), $@"d:\!Temp\bic.png"));
+        }
+
+        Rectangle FullScreenBounds;
+
+        private void SetFullScreen(bool value)
+        {
+            if (miMainFullScreen.Checked == value) return;
+
+            miMainFullScreen.Checked = value;
+
+            TopMost = value;
+
+            if (value)
+            {
+                FormBorderStyle = FormBorderStyle.None;
+
+                toolStripContainer.TopToolStripPanel.Visible = false;
+                toolStripContainer.BottomToolStripPanel.Visible = false;
+
+                FullScreenBounds = Bounds;
+
+                SetBounds(0, 0, Screen.FromControl(this).Bounds.Width, Screen.FromControl(this).Bounds.Height);
+            }
+            else
+            {
+                FormBorderStyle = FormBorderStyle.Sizable;
+
+                toolStripContainer.TopToolStripPanel.Visible = true;
+                toolStripContainer.BottomToolStripPanel.Visible = true;
+
+                Bounds = FullScreenBounds;
+            }
+        }
+
+        private void MiMainFullScreen_Click(object sender, EventArgs e)
+        {
+            SetFullScreen(!miMainFullScreen.Checked);
+        }
+
+        private void MiMainAbout_Click(object sender, EventArgs e)
+        {
+            var assembly = Assembly.GetExecutingAssembly();
+
+            var assemblyTitle = (AssemblyTitleAttribute)assembly.GetCustomAttribute(typeof(AssemblyTitleAttribute));
+            var assemblyVersion = (AssemblyFileVersionAttribute)assembly.GetCustomAttribute(typeof(AssemblyFileVersionAttribute));
+            var assemblyCopyright = (AssemblyCopyrightAttribute)assembly.GetCustomAttribute(typeof(AssemblyCopyrightAttribute));
+
+            var text = assemblyTitle.Title + Environment.NewLine +
+                assemblyVersion.Version + Environment.NewLine +
+                assemblyCopyright.Copyright;
+
+            MessageBox.Show(text, Application.ProductName, MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+        private void gMapControl_OnMarkerClick(GMapMarker item, MouseEventArgs e)
+        {
+            MessageBox.Show(item.ToString(), Application.ProductName, MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+    }
+}
