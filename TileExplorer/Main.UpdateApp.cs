f42a85ca5e2a01198adf2a73f92369faf9839d69
Константин Петрович <P3tr0viCh@users.noreply.github.com>
2024-06-25 11:16:55 +0500

    ~ UpdateApp.

--- TileExplorer/Main.UpdateApp.cs
+++ TileExplorer/Main.UpdateApp.cs
@@ -8,100 +8,97 @@ using TileExplorer.Properties;
 
 namespace TileExplorer
 {
-    public partial class Main
+    internal class UpdateApp
     {
-        private class UpdateApp
+        private const string GitHubOwner = "P3tr0viCh";
+        private const string GitHubRepo = "TileExplorer";
+        private const string GitHubArchiveFile = "latest.zip";
+
+        private static readonly UpdateApp instance = new UpdateApp();
+        public static UpdateApp Default => instance;
+
+        public event ProgramStatus<UpdateStatus>.StatusChangedEventHandler StatusChanged;
+
+        private AppUpdate AppUpdate = null;
+
+        public bool CanClose()
         {
-            private const string GitHubOwner = "P3tr0viCh";
-            private const string GitHubRepo = "TileExplorer";
-            private const string GitHubArchiveFile = "latest.zip";
-
-            private static readonly UpdateApp instance = new UpdateApp();
-            public static UpdateApp Default => instance;
-
-            public event ProgramStatus<UpdateStatus>.StatusChangedEventHandler StatusChanged;
-
-            private AppUpdate AppUpdate = null;
-
-            public bool CanClose()
+            if (AppUpdate == null)
             {
-                if (AppUpdate == null)
-                {
-                    return true;
-                }
-
-                return AppUpdate.Status.IsIdle();
+                return true;
             }
 
-            public async void Update()
+            return AppUpdate.Status.IsIdle();
+        }
+
+        public async void Update()
+        {
+            if (AppUpdate != null)
             {
-                if (AppUpdate != null)
+                Msg.Info(Resources.AppUpdateInfoInProgress);
+
+                return;
+            }
+
+            AppUpdate = new AppUpdate();
+
+            AppUpdate.Config.LocalFile = Application.ExecutablePath;
+
+            AppUpdate.Config.Location = Location.GitHub;
+
+            AppUpdate.Config.GitHub.Owner = GitHubOwner;
+            AppUpdate.Config.GitHub.Repo = GitHubRepo;
+            AppUpdate.Config.GitHub.ArchiveFile = GitHubArchiveFile;
+
+            AppUpdate.Status.StatusChanged += StatusChanged;
+
+            var errorMsg = string.Empty;
+
+            try
+            {
+                AppUpdate.Check();
+
+                await AppUpdate.CheckLatestVersionAsync();
+
+                if (AppUpdate.Versions.IsLatest())
                 {
-                    Msg.Info(Resources.AppUpdateInfoInProgress);
+                    Msg.Info(Resources.AppUpdateInfoAlreadyLatest);
 
                     return;
                 }
 
-                AppUpdate = new AppUpdate();
+                await AppUpdate.UpdateAsync();
 
-                AppUpdate.Config.LocalFile = Application.ExecutablePath;
+                Msg.Info(Resources.AppUpdateInfoUpdated);
+            }
+            catch (LocalFileWrongLocationException)
+            {
+                var path = Path.Combine(Directory.GetParent(AppUpdate.Config.LocalFile).FullName,
+                    AppUpdate.Versions.Local.ToString(),
+                    Path.GetFileName(AppUpdate.Config.LocalFile));
 
-                AppUpdate.Config.Location = P3tr0viCh.AppUpdate.Location.GitHub;
+                errorMsg = string.Format(Resources.AppUpdateErrorFileWrongLocation, path);
+            }
+            catch (HttpRequestException e)
+            {
+                DebugWrite.Error(e);
 
-                AppUpdate.Config.GitHub.Owner = GitHubOwner;
-                AppUpdate.Config.GitHub.Repo = GitHubRepo;
-                AppUpdate.Config.GitHub.ArchiveFile = GitHubArchiveFile;
+                errorMsg = e.Message;
+            }
+            catch (Exception e)
+            {
+                DebugWrite.Error(e);
 
-                AppUpdate.Status.StatusChanged += StatusChanged;
+                errorMsg = e.Message;
+            }
+            finally
+            {
+                AppUpdate = null;
+            }
 
-                var errorMsg = string.Empty;
-
-                try
-                {
-                    AppUpdate.Check();
-
-                    await AppUpdate.CheckLatestVersionAsync();
-
-                    if (AppUpdate.Versions.IsLatest())
-                    {
-                        Msg.Info(Resources.AppUpdateInfoAlreadyLatest);
-
-                        return;
-                    }
-
-                    await AppUpdate.UpdateAsync();
-
-                    Msg.Info(Resources.AppUpdateInfoUpdated);
-                }
-                catch (LocalFileWrongLocationException)
-                {
-                    var path = Path.Combine(Directory.GetParent(AppUpdate.Config.LocalFile).FullName,
-                        AppUpdate.Versions.Local.ToString(),
-                        Path.GetFileName(AppUpdate.Config.LocalFile));
-
-                    errorMsg = string.Format(Resources.AppUpdateErrorFileWrongLocation, path);
-                }
-                catch (HttpRequestException e)
-                {
-                    DebugWrite.Error(e);
-
-                    errorMsg = e.Message;
-                }
-                catch (Exception e)
-                {
-                    DebugWrite.Error(e);
-
-                    errorMsg = e.Message;
-                }
-                finally
-                {
-                    AppUpdate = null;
-                }
-
-                if (!errorMsg.IsEmpty())
-                {
-                    Msg.Error(Resources.AppUpdateErrorInProgress, errorMsg);
-                }
+            if (!errorMsg.IsEmpty())
+            {
+                Msg.Error(Resources.AppUpdateErrorInProgress, errorMsg);
             }
         }
     }
