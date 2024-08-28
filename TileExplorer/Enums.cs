7f5d9b45bcf37da9fc14fe2652fcc74cdfa2c7d7
Константин Петрович <P3tr0viCh@users.noreply.github.com>
2024-08-28 08:48:34 +0500

    ~ fix.

--- TileExplorer/Enums.cs
+++ TileExplorer/Enums.cs
@@ -1,25 +1,11 @@
 ﻿using System;
 using System.ComponentModel;
-using System.Linq;
 using static P3tr0viCh.Utils.Converters;
 
 namespace TileExplorer
 {
     public static class Enums
     {
-        public static string Description(this Enum value)
-        {
-            var field = value.GetType().GetField(value.ToString());
-
-            if (field.GetCustomAttributes(typeof(DescriptionAttribute), false) is DescriptionAttribute[] attributes
-                && attributes.Any())
-            {
-                return attributes.First().Description;
-            }
-
-            return value.ToString();
-        }
-
         [TypeConverter(typeof(EnumDescriptionConverter))]
         public enum Status
         {
